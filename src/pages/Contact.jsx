b985fdfc4ba3ed2b58f8119fa826861323ce45dd
NgaLeLe <le.thuy.nga@gmail.com>
2022-03-18 15:00:33 +0100

    contact-V2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,10 +8,10 @@ import {
 function Contact() {
     return (
         <div className="contactContainer" >
-            <Container className='bg-light ' style={{width:'60%', marginTop:'30px', padding:'20px' }}>
+            <Container className='bg-light ' style={{width:'60%', marginTop:'10px', padding:'20px' }}>
                 <h3  style={{textAlign: 'center', height:'50px', backgroundColor:'#ffc107', alignItems: 'center', paddingTop:'7px'}}>Envoyez-nous votre opinion</h3>
-                <Form>
-                    <Row className="mb-3">
+                <Form  className="mb-2 mt-2 ">
+                    <Row >
                         <Form.Group as={Col} controlId="formGridFirstName">
                           <Form.Label>Prénom</Form.Label>
                           <Form.Control placeholder="votre prénom" />
@@ -22,23 +22,41 @@ function Contact() {
                         </Form.Group>
                     </Row>
 
-                    <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Group controlId="formGridEmail">
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email" placeholder="Saisis votre email" />
-                    </Form.Group            >
-                    <Form.Group className="mb-3" controlId="formGridMessage">
-                        <Form.Label>Votre message</Form.Label>
-                        <Form.Control placeholder="..." />
                     </Form.Group >
-        
-                    <Form.Group className="mb-3" id="formGridCheckbox">
-                        <Form.Check  type="checkbox" label="Souvenir-moi" />
-                    </Form.Group>
 
-                    <Button variant="outline-warning" type="submit" style={{alignItems: 'center'}}>
+                    <Form.Group controlId="formGridSujet">
+                        <Form.Label >Sujet</Form.Label>
+                        <Form.Control   />
+                    </Form.Group>
+                    
+                    <Form.Group className="mb-2" controlId="formGridMessage" >
+                        <Form.Label>Votre message</Form.Label>
+                        <Form.Control as="textarea" placeholder="..." />
+                    </Form.Group >
+
+                    <Button variant="outline-warning" type="submit" style={{alignItems: 'center'}} >
                         Envoyer
                     </Button>
                 </Form>
+            </Container>
+            <Container className='bg-dark ' style={{width:'60%', marginTop:'10px',  padding:'15px' }}>
+                <Row>
+                    <Col className="myAddress" style={{alignContent: 'center'}}>
+                        <h6 className='text-warning'>Address</h6>
+                        <p className='text-light'>Web Force 3 - Paris</p>
+                    </Col>
+                    <Col className="myNumber">
+                        <h6 className='text-warning'>Téléphone</h6>
+                        <p className='text-light'>+ 33334567</p>
+                    </Col>
+                    <Col className="address">
+                        <h6 className='text-warning'>Email</h6>
+                        <p className='text-light'>WF3_group6@gmail.com</p>
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
