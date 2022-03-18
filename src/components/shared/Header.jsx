287ed83ba5f4ef6618c799327caf769609bdfd72
NgaLeLe <le.thuy.nga@gmail.com>
2022-03-18 11:13:38 +0100

    update fichier Header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -10,13 +10,14 @@ function Header() {
         <header>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">BLOG</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/about">About us</Nav.Link>
+                            <Nav.Link href="/contact">Contact</Nav.Link>
+                            <NavDropdown title="Blog" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
