46c05198806804cc3988170e66a8fce2d8876dc7
NgaLeLe <le.thuy.nga@gmail.com>
2022-03-18 15:36:36 +0100

    footer-v1

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,17 @@
+import {Container,
+    Row,
+    Col
+} from 'react-bootstrap';
 function Footer() {
     return (
         <footer>
-            
+            <Container>
+                <Row id="info"></Row>
+                <Row id="copyright">
+                    <p>© 2023 by Cuisine du mone.
+Créer par Anzor, Hugo & Nga</p>
+                </Row>
+            </Container>
         </footer>
     );
 }
